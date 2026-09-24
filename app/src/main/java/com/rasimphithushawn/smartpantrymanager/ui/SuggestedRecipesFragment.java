0c945c1361487ffbf7fe91903eb3ee4317807fd0
rasimphithushawn-eng <rasimphithushawn@gmail.com>
2026-09-24 12:03:18 +0200

    Add placeholder fragments for suggestions and settings

---
+++ app/src/main/java/com/rasimphithushawn/smartpantrymanager/ui/SuggestedRecipesFragment.java
@@ -0,0 +1,17 @@
+package com.rasimphithushawn.smartpantrymanager.ui;
+
+import android.os.Bundle;
+import android.view.*;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+public class SuggestedRecipesFragment extends Fragment {
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup c, Bundle b) {
+        TextView tv = new TextView(getContext());
+        tv.setText("Suggestions coming on Day 3...");
+        tv.setPadding(40, 60, 40, 40);
+        return tv;
+    }
+}
